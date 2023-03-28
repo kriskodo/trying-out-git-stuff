c56f1b24a476b531d7d3ef4608eb73237bcaa678
Kristian Georgiev <70609099+kriskodo@users.noreply.github.com>
2023-03-28 21:28:31 +0300

    TOF-1 [remove] some test cases (#1)
    * TOF-1 [delete] test case
    
    * TOF-1 [remove] another test case and commit

--- src/features/counter/counterSlice.spec.ts
+++ src/features/counter/counterSlice.spec.ts
@@ -21,14 +21,4 @@ describe('counter reducer', () => {
     const actual = counterReducer(initialState, increment());
     expect(actual.value).toEqual(4);
   });
-
-  it('should handle decrement', () => {
-    const actual = counterReducer(initialState, decrement());
-    expect(actual.value).toEqual(2);
-  });
-
-  it('should handle incrementByAmount', () => {
-    const actual = counterReducer(initialState, incrementByAmount(2));
-    expect(actual.value).toEqual(5);
-  });
 });
