be3fdc03e29b10156634b5e9e4c375bce18b0d12
HDElemental <kristiangeorgiev99ss@abv.bg>
2023-03-28 21:26:01 +0300

    TOF-1 [delete] test case

--- src/features/counter/counterSlice.spec.ts
+++ src/features/counter/counterSlice.spec.ts
@@ -22,11 +22,6 @@ describe('counter reducer', () => {
     expect(actual.value).toEqual(4);
   });
 
-  it('should handle decrement', () => {
-    const actual = counterReducer(initialState, decrement());
-    expect(actual.value).toEqual(2);
-  });
-
   it('should handle incrementByAmount', () => {
     const actual = counterReducer(initialState, incrementByAmount(2));
     expect(actual.value).toEqual(5);
